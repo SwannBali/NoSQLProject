7d18dcb526d78a7161634566766d7f7d0489b236
Swann_Bali <Swannbld@gmail.com>
2024-02-28 18:53:22 +0100

    fix expiration

--- nosql-back/app/Controllers/Http/ProductsController.ts
+++ nosql-back/app/Controllers/Http/ProductsController.ts
@@ -61,23 +61,26 @@ export default class ProductsController {
 
     public async search({ request, response }: HttpContextContract) {
         const searchTerm = request.input("searchTerm")
-        const allKeys = await Redis.hkeys("produits")
+        const allKeys = await Redis.hkeys("produits") // Ici on récupère toutes les clés de notre base de données Redis
 
-        // await Redis.setex("/test", 60, "test")
+        const matchingKeys = allKeys.filter((key) => key.startsWith(searchTerm)) // Ici on filtre les clés qui commencent par le terme de recherche
 
-        const matchingKeys = allKeys.filter((key) => key.startsWith(searchTerm))
+        const jsonResult = await Redis.hget("produits", matchingKeys[0]) // Ici on récupère le résultat de la recherche dans notre base de données Redis
 
-        const jsonResult = await Redis.hget("produits", matchingKeys[0])
-
-        Logger.info("before redis")
-
-        if (jsonResult) return response.json(JSON.parse(jsonResult))
+        if (jsonResult) return response.json(JSON.parse(jsonResult)) // Ici on retourne le résultat de la recherche si on le trouve dans notre base de données Redis
 
         const results = await Product.find({
+            // Ici on fait la recherche dans notre base de données MongoDB si on ne trouve pas le résultat dans notre base de données Redis
             name: { $regex: new RegExp(searchTerm, "i") },
         })
 
-        await Redis.hsetnx("produits", searchTerm, JSON.stringify(results))
+        if (allKeys.length >= 5) {
+            await Redis.hdel("produits", String(allKeys.reverse()[0])) // Ici on supprime la première clé de notre base de données Redis si on a déjà 5 clés
+        }
+
+        await Redis.hsetnx("produits", searchTerm, JSON.stringify(results)) // Ici on stocke le résultat de la recherche dans notre base de données Redis
+
+        await Redis.expire("produits", 3600) // Ici on définit une expiration de 1 heure pour notre base de données Redis
 
         return response.json(results)
     }
